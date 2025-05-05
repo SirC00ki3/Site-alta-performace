246a21e361144c61ed6f86f0c3d7dbcd2622f4f5
SirC00ki3 <caioraul36@gmail.com>
2025-05-05 12:25:00 -0300

    css do cadastro atualizado jutno com js dele tbm

---
+++ cadastro.js
@@ -0,0 +1,38 @@
+document.getElementById('singup-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    const nome = document.getElementById('Nome').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const senha = document.getElementById('password').value;
+    const erro = document.getElementById('error-message');
+
+    // Validação de e-mail com regex
+    const emailValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  
+    // Validação de senha forte: mínimo 8 caracteres, 1 letra maiúscula e 1 número
+    const senhaForte = /^(?=.*[A-Z])(?=.*\d).{8,}$/.test(senha);
+  
+    // Verificações
+    if (nome.length < 4) {
+        erro.textContent = 'O nome deve ter pelo menos 4 letras.';
+        return;
+      }
+  
+    if (!emailValido) {
+      erro.textContent = 'E-mail inválido.';
+      return;
+    }
+  
+    if (!senhaForte) {
+      erro.textContent = 'A senha deve ter no mínimo 8 caracteres, com uma letra maiúscula e um número.';
+      return;
+    }
+  
+    // Se tudo estiver válido
+    erro.textContent = '';
+    alert('Cadastro válido!');
+
+    window.location.href = 'login.html';
+
+  });
+  
