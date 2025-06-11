398e48983276717de335c06424092880adccc0f8
Cv1ana <caioraul36@gmail.com>
2025-06-11 16:22:07 -0300

    ajuda

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { //Aguarda carregamento da página:
   
   const loginForm = document.getElementById('login-form');
   const toggleBtn = document.getElementById('toggle-theme');
   const body = document.body;
-  const errorMessage = document.getElementById('error-message');
+  const errorMessage = document.getElementById('error-message'); //Elementos principais
 
-  // Função para aplicar tema salvo e atualizar o ícone
+
   const aplicarTemaSalvo = () => {
     const savedTheme = localStorage.getItem('theme');
     if (savedTheme === 'dark') {
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
       body.classList.remove('dark-theme');
       toggleBtn.textContent = '☀️';
     }
-  };
+  };  // Função para aplicar tema salvo e atualizar o ícone
 
   aplicarTemaSalvo();
 
@@ -31,9 +31,9 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  // Lógica de login
+  
   loginForm.addEventListener('submit', function (e) {
-    e.preventDefault();
+    e.preventDefault(); //Evita o envio normal do formulário(validar os if-else)
 
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value.trim();
@@ -41,11 +41,11 @@ document.addEventListener('DOMContentLoaded', () => {
     if (!email || !password) {
       errorMessage.textContent = 'Por favor, preencha todos os campos.';
       return;
-    }
+    } //Verifica campos vazios.
 
-    const usuariosSalvos = JSON.parse(localStorage.getItem('usuarios')) || [];
+    const usuariosSalvos = JSON.parse(localStorage.getItem('usuarios')) || []; Busca usuários salvos.
 
-    const usuarioEncontrado = usuariosSalvos.find(user => user.email === email && user.senha === password);
+    const usuarioEncontrado = usuariosSalvos.find(user => user.email === email && user.senha === password); //Procura o usuário com email e senha corretos.
 
     if (usuarioEncontrado) {
       const sessionUser = { email: usuarioEncontrado.email, name: usuarioEncontrado.nome };
@@ -56,4 +56,4 @@ document.addEventListener('DOMContentLoaded', () => {
       errorMessage.textContent = 'E-mail ou senha incorretos.';
     }
   });
-});
+}); //Se achou o usuário: salva sessão e redireciona para perfil. Caso contrário: mostra erro.
